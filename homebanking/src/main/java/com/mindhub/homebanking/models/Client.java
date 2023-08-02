915111b25c069d54202cb0c703df46d20650e171
Leandro Maselli <leoroan@gmail.com>
2023-08-02 17:42:14 -0300

    agregado models y repos

---
+++ homebanking/src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,55 @@
+package com.mindhub.homebanking.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+  private long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public Client() {
+  }
+
+  public Client(String first, String last, String email) {
+    this.firstName = first;
+    this.lastName = last;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String toString() {
+    return firstName + " " + lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
