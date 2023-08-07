9b4f2e07330d2ff5a2da7e2840116c1577f637d1
Leandro <leoroan@gmail.com>
2023-08-07 20:43:17 -0300

    continuando las task 2

--- homebanking-T2/src/main/java/com/mindhub/homebanking/models/Account.java
+++ homebanking-T2/src/main/java/com/mindhub/homebanking/models/Account.java
@@ -1,2 +1,56 @@
-package com.mindhub.homebanking.models;public class Account {
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String number;
+    private LocalDate creationDate;
+    private double balance;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Account() {
+    }
+
+    public Account(String number, LocalDate creationDate, double balance, Client client) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+        this.client = client;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+
 }
--- homebanking-T2/src/main/java/com/mindhub/homebanking/repositories/ClientRepository.java
+++ homebanking-T2/src/main/java/com/mindhub/homebanking/repositories/ClientRepository.java
@@ -1,9 +1,8 @@
 package com.mindhub.homebanking.repositories;
 
+import com.mindhub.homebanking.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import com.mindhub.homebanking.models.Client;
 
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long> {
