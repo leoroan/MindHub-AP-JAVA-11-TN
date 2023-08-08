703e64a1f8da93443e979f8ce277416a1775aef2
Leandro <leoroan@gmail.com>
2023-08-07 21:19:04 -0300

    creada la clase  ClienController y definida

--- homebanking-T2/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ homebanking-T2/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -1,8 +1,26 @@
 package com.mindhub.homebanking.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+
+@RestController
+@RequestMapping("/api")
 public class ClientController {
+
+  @Autowired
+  private ClientRepository clientRepository;
+
+  @RequestMapping("/clients")
+  public List<Client> getClients() {
+    return clientRepository.findAll();
+
+  }
 
 }
