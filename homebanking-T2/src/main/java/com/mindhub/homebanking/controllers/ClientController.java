0c27632df3f6d1af062f562e3e315fd053f9b7c0
Leandro <leoroan@gmail.com>
2023-08-07 21:20:18 -0300

    eliminado import

--- homebanking-T2/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ homebanking-T2/src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 
