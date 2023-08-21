47cda5505c7bc1364db232dcda123e28f6ccdc70
Leandro <leoroan@gmail.com>
2023-08-21 16:18:41 -0300

    some fixes

--- homebanking-T6/src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ homebanking-T6/src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,60 +25,63 @@ import org.springframework.security.web.authentication.logout.HttpStatusReturnin
  */
 class WebAuthorization {
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    /**
-     * Se debe tener en cuenta que el orden de las reglas es importante ya que en
-     * este caso si se recibe una
-     * petición con la URL /data, se verifica primero contra el
-     * antMatcher(“/admin/**”) y si este no se cumple
-     * entonces pasa al siguiente, por lo que se valida contra antMatcher(“/**”) y
-     * como se cumple entonces
-     * Spring Security verificará que exista una sesión iniciada y que ese usuario
-     * tenga el rol USER para poder
-     * acceder a /data.
-     */
-    http.authorizeRequests()
-        .antMatchers("/admin/**").hasAuthority("ADMIN")
-        .antMatchers("/rest/**").hasAuthority("ADMIN")
-        .antMatchers("/h2-console/**").hasAuthority("ADMIN")
-        .antMatchers("/**").hasAuthority("CIENT");
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http.formLogin()
-        .usernameParameter("name")
-        .passwordParameter("pwd")
-        .loginPage("/api/login");
+        /**
+         * Se debe tener en cuenta que el orden de las reglas es importante ya que en
+         * este caso si se recibe una
+         * petición con la URL /data, se verifica primero contra el
+         * antMatcher(“/admin/**”) y si este no se cumple
+         * entonces pasa al siguiente, por lo que se valida contra antMatcher(“/**”) y
+         * como se cumple entonces
+         * Spring Security verificará que exista una sesión iniciada y que ese usuario
+         * tenga el rol USER para poder
+         * acceder a /data.
+         */
+        http.authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/**").permitAll();
+                .antMatchers("/**").permitAll();
+//                .antMatchers("/admin/**").hasAuthority("ADMIN")
+//                .antMatchers("/rest/**").hasAuthority("ADMIN")
+//                .antMatchers("/h2-console/**").hasAuthority("ADMIN")
+//                .antMatchers("/**").hasAuthority("CLIENT");
 
-    http.logout().logoutUrl("/api/logout");
+        http.formLogin()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginPage("/api/login");
 
-    // turn off checking for CSRF tokens
+        http.logout().logoutUrl("/api/logout");
 
-    http.csrf().disable();
+        // turn off checking for CSRF tokens
 
-    // disabling frameOptions so h2-console can be accessed
-    http.headers().frameOptions().disable();
+        http.csrf().disable();
 
-    // if user is not authenticated, just send an authentication failure response
-    http.exceptionHandling()
-        .authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+        // disabling frameOptions so h2-console can be accessed
+        http.headers().frameOptions().disable();
 
-    // if login is successful, just clear the flags asking for authentication
-    http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+        // if user is not authenticated, just send an authentication failure response
+        http.exceptionHandling()
+                .authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
 
-    // if login fails, just send an authentication failure response
-    http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+        // if login is successful, just clear the flags asking for authentication
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
 
-    // if logout is successful, just send a success response
-    http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
-    return http.build();
+        // if login fails, just send an authentication failure response
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
 
-  }
+        // if logout is successful, just send a success response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+        return http.build();
 
-  private void clearAuthenticationAttributes(HttpServletRequest request) {
-    HttpSession session = request.getSession(false);
-    if (session != null) {
-      session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
     }
-  }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
+    }
 }
